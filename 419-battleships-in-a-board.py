7412b792f0a7d68d134733ab8e62e5ff5e91e41c
webwesen <webwesen@webwesen.com>
2020-07-26 12:48:31 -0500

    Create 419-battleships-in-a-board.py

---
+++ 419-battleships-in-a-board.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/battleships-in-a-board/
+
+class Solution:
+    def countBattleships(self, board: List[List[str]]) -> int:
+        if not board: 
+            return 0
+        
+        num_ships = 0
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                # print(i, j)
+                if board[i][j] == "X":
+                    num_ships += 1
+                    self.sink_ship(board, i, j)
+                
+        return(num_ships)
+    
+    def sink_ship(self, board, i, j):
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] == '.':
+            return
+        else:
+            board[i][j] = "."
+            self.sink_ship(board, i-1, j)
+            self.sink_ship(board, i+1, j)
+            self.sink_ship(board, i, j-1)
+            self.sink_ship(board, i, j+1)
+        return
